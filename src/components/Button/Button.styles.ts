6b79ddeb2985407a3b1cd984e340bac2e8c38571
rodrigokovalski <rodrigokvk@gmail.com>
2025-03-27 07:28:58 -0300

    agr vai

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,6 +1,4 @@
 import styled from 'styled-components'
 import { ButtonProps } from './Button.types'
 
-export const StyledButton = styled.button<ButtonProps>`
-    background-color: ${(props) => (props.isLoading ? `#000` : `white`)};
-`
+export const StyledButton = styled.button<ButtonProps>``
